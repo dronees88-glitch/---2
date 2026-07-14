5becf1f3a9e275f6f7307f3cb4023321166cb09f
dronees88-glitch <dronees88@gmail.com>
2026-07-14 16:59:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Student
+{
+public:
+    void show()
+    {
+        cout << "Это студент" << endl;
+    }
+};
+
+class ExcellentStudent : public Student
+{
+public:
+    void info()
+    {
+        cout << "Учится на отлично" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Student s;
+    ExcellentStudent e;
+
+    cout << "Базовый класс:" << endl;
+    s.show();
+
+    cout << endl;
+
+    cout << "Производный класс:" << endl;
+    e.show();
+    e.info();
+
+    system("pause");
+    return 0;
+}
